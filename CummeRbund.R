50269a6c4bb5b4df7070c6e49d9e82e524a8095d
Selaginella_V <veronica9907@sina.com>
2015-01-25 14:15:08 -0500

    Update CummeRbund.R

--- CummeRbund.R
+++ CummeRbund.R
@@ -16,7 +16,7 @@ den = csDensity(genes(cuff_data))
 sca = csScatter(genes(cuff_data),'WT_G1','KO_G1')
 plot(sca)
 
-# Create a volcano plot to inspect differentially expressed genes,
+# Create a volcano plot to inspect differentially expressed genes, to see whether it is significantly different
 vol = csVolcano(genes(cuff_data), 'WT_G1','KO_G1')
 plot(vol)
 
@@ -27,4 +27,29 @@ expressionBarplot(mygene)
 # Plot individual isoform expression levels of selected genes of interest with bar plots
 expresssionBarplot(isoforms(mygene))
 
+###################################################################################################
+# hierarchical clustering of differential expressed genes in three time conditions
 
+G1 = readCufflinks('G1_diff')
+G2 = readCufflinks('G2_diff')
+G3 = readCufflinks('G3_diff')
+
+gene1.matrix = fpkmMatrix(genes(G1))
+gene2.matrix = fpkmMatrix(genes(G2))
+gene3.matrix = fpkmMatrix(genes(G3))
+
+# create a gene list
+library(gplots)
+alldiff3 = read.delim(alldiff3.txt, header = FALSE, sep = "\t")
+# getGenes(G1, t(alldiff3))
+# gene1.matrix = fpkmMatrix()
+gene1.matrix = fpkmMatrix(getGenes(G1, t(alldiff3))) 
+gene2.matrix = fpkmMatrix(getGenes(G2, t(alldiff3))) 
+gene3.matrix = fpkmMatrix(getGenes(G3, t(alldiff3))) 
+temp = cbind(gene1.matrix, gene2.matrix, gene3.matrix)
+heatmap.2(as.matrix(temp), scale="row", dendrogram="both", density.info=c("none"), margins=c(5,5), trace=c("none"), labRow=F)
+# functions like distfun = dist, hclustfun = hclust...
+
+#change to pretty color
+library("RColorBrewer")
+heatmap.2(as.matrix(temp), scale="row", dendrogram="both", density.info=c("none"), margins=c(5,5), trace=c("none"), labRow=F, col)
